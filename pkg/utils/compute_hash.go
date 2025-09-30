b55862324e91b2919e8f1d9c28215adfcecd44a8
Nils Borg <nils.ml.borg@gmail.com>
2025-09-30 15:04:34 +0200

    Add tests for computing hash

--- pkg/utils/compute_hash.go
+++ pkg/utils/compute_hash.go
@@ -7,8 +7,5 @@ func ComputeHash(data string, length int) *BitArray {
 	// Sha256 hash and truncate to desired length in bits
 	hash := sha256.Sum256([]byte(data))
 
-	if length > len(hash) {
-		length = len(hash)
-	}
-	return NewBitArrayFromBytes(hash[:length], length*8)
+	return NewBitArrayFromBytes(hash[:], length)
 }
